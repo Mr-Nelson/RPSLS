6d909b0ac710f99176c47923f3472a775b6fba0f
Mr-Nelson <kdnels88@gmail.com>
2021-06-16 11:02:04 -0500

    showdown method loops through if statements, but doesn't exit method

--- center_stage.py
+++ center_stage.py
@@ -12,8 +12,6 @@ class Center_Stage:
         # Instructions
 
         self.choose_game_mode()
-        self.player_one_choice()
-        self.player_two_choice()
         self.showdown()
         self.display_winner()
 
@@ -27,6 +25,28 @@ class Center_Stage:
             self.player_two = Human(input("Enter player two's name."))
         else:
             self.player_two = Artificial_Intelligence("Chappie")
+
+    def showdown(self):
+        # choices switched to integers [rock=0, paper=1, scissors=2, lizard=3, spock=4]
+        while (0 > 2 and 0 > 3, 2 > 1 and 2 > 3, 1 > 0 and 1 > 4, 3 > 4 and 3 > 1, 4 > 2 and 4 > 0):
+            while self.player_one.score < 2 and self.player_two.score < 2:
+                self.player_one_choice()
+                self.player_two_choice()
+                if self.player_one.chosen_gesture > self.player_two.chosen_gesture:
+                    self.player_one.score += 1
+                    print(f"{self.player_one.name} won with {self.player_one.chosen_gesture}!")
+                    print(f"{self.player_one.name}:{self.player_one.score} {self.player_two.name}:{self.player_two.score}")
+                    self.showdown()
+                if self.player_one.chosen_gesture < self.player_two.chosen_gesture:
+                    self.player_two.score += 1
+                    print(f"{self.player_two.name} won with {self.player_two.chosen_gesture}!")
+                    print(f"{self.player_one.name}:{self.player_one.score} {self.player_two.name}:{self.player_two.score}")
+                    self.showdown()
+                if self.player_one.chosen_gesture == self.player_two.chosen_gesture:
+                    print("It was a tie!")
+                    self.player_one_choice()
+                    self.player_two_choice()
+                    self.showdown()
 
     def player_one_choice(self):
         self.player_one.choose_gesture()
@@ -42,19 +62,6 @@ class Center_Stage:
         else:
             self.player_two.choose_gesture()
             print(f"{self.player_two.name} chooses {self.player_two.chosen_gesture}.")
-
-    def showdown(self):
-        # choices switched to integers [rock=0, paper=1, scissors=2, lizard=3, spock=4]
-        while self.player_one.score < 2 and self.player_two.score < 2:
-            while (0 > 2 and 0 > 3, 2 > 1 and 2 > 3, 1 > 0 and 1 > 4, 3 > 4 and 3 > 1, 4 > 2 and 4 > 0):
-                if self.player_one.chosen_gesture > self.player_two.chosen_gesture:
-                    self.player_one.score += 1
-                    # print(f"{self.player_one.name} won with {self.player_one.chosen_gesture}!")
-                    # print(f"{self.player_one.name}:{self.player_one.score} {self.player_two.name}:{self.player_two.score}")
-                if self.player_one.chosen_gesture < self.player_two.chosen_gesture:
-                    self.player_two.score += 1
-                    # print(f"{self.player_two.name} won with {self.player_two.chosen_gesture}!")
-                    # print(f"{self.player_one.name}:{self.player_one.score} {self.player_two.name}:{self.player_two.score}")
 
     def display_winner(self):
         if self.player_one.score == 2:
